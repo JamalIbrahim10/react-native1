354abc3a9bceb19a8e159e54039e19841ad66c89
Jamal Ibrahim <jamalibrahim@Jamals-MacBook-Pro.local>
2020-08-12 10:37:20 -0700

    Week1 Worshop

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,12 +4,14 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
-    }, 
+    },
     {
         initialRouteName: 'Directory',
         navigationOptions: {
@@ -23,7 +25,6 @@ const DirectoryNavigator = createStackNavigator(
         }
     }
 );
-
 
 const HomeNavigator = createStackNavigator(
     {
@@ -42,17 +43,51 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator },
+        Contact: { screen: ContactNavigator}
     },
     {
         drawerBackgroundColor: '#CEC8FF'
     }
 );
-
-
 
 class Main extends Component {
     render() {
